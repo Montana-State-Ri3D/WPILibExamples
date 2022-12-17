6a39f62ac2ca66675dd7ece39182bf88a9bd19ad
Richard Elmore <richardelmore@users.noreply.github.com>
2022-12-16 22:53:42 -0800

    Cleanup and add vendor deps

--- Examples/src/main/java/frc/robot/RobotContainer.java
+++ Examples/src/main/java/frc/robot/RobotContainer.java
@@ -1,40 +1,47 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and button mappings) should be declared here.
- */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  @SuppressWarnings({"unused"})
+  private final XboxController driveController = new XboxController(Constants.DRIVE_CONTROLLER_PORT); 
+  @SuppressWarnings({"unused"})
+  private final XboxController operatorController = new XboxController(Constants.OPERATOR_CONTROLLER_PORT);
+  @SuppressWarnings({"unused"})
+  private final XboxController testController = new XboxController(Constants.TEST_CONTROLLER_PORT);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  private final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
+
+  private final ExampleCommand autoCommand = new ExampleCommand(exampleSubsystem);
+
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
   public RobotContainer() {
-    // Configure the button bindings
+
+    createSubsystems();
+    createCommands();
     configureButtonBindings();
   }
 
   /**
-   * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
-   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   * Create all of our robot's subsystem objects here.
    */
-  private void configureButtonBindings() {}
+  private void createSubsystems() {
+  }
+
+  /**
+   * Create all of our robot's command objects here.
+   */
+  private void createCommands() {
+  }
+
+  private void configureButtonBindings() {
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
@@ -43,6 +50,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+    return autoCommand;
   }
 }
